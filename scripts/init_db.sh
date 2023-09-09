973cc873e1909f36fa115adac1d421d14aff1a98
ārXāngel MacBook <lavrenyk@gmail.com>
2023-09-10 08:32:25 +1000

    project structure refactor

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -18,25 +18,23 @@ if ! [ -x "$(command -v sqlx)" ]; then
   exit 1
 fi
 
-# Check if a custome user has been set, otherwise default to 'postgres'
+# Setup constants
 DB_USER="${POSTGRESS_USER:=postgres}"
-# Check if a custome password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custome database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
-# Check if a custome port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5435}"
-# Check if a custome host has been set, otherwise default location to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Launch postgres using Docker
-docker run\
- -e POSTGRES_USER=${DB_USER}\
- -e POSTGRES_PASSWORD=${DB_PASSWORD}\
- -e POSTGRES_DB=${DB_NAME}\
- -p "${DB_PORT}":5432\
- -d postgres -N 1000
-# ^ Increased maximum number of connextions for testing purpose
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run\
+  -e POSTGRES_USER=${DB_USER}\
+  -e POSTGRES_PASSWORD=${DB_PASSWORD}\
+  -e POSTGRES_DB=${DB_NAME}\
+  -p "${DB_PORT}":5432\
+  -d postgres -N 1000
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -50,3 +48,6 @@ done
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 //! lib.rs
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
