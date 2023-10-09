7d0415ae0887c161df3b1bf5ad17fdbeb74430b8
ārXāngel MacBook <lavrenyk@gmail.com>
2023-10-09 11:19:03 +0300

    sqlx data offline set

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -22,6 +22,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     );
     let _request_span_guard = request_span.enter();
     let query_span = tracing::info_span!("Saving new subscriber details in the database.");
+    let timestamp = Utc::now();
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -30,7 +31,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Uuid::new_v4(),
         form.email,
         form.name,
-        Utc::now()
+        timestamp
     )
     // We use `get_ref` to get an immutable reference to the PgPool
     // wrapped by `web::Data`
