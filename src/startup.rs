555062261f930b6f4d911a5102fb2822db9ba5cb
ārXāngel MacBook <lavrenyk@gmail.com>
2023-11-01 00:06:02 +0300

    startup change

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
 //! src/startup.rs
-use crate::routes::{get_animal_by_period, health_check, subscribe};
+use crate::routes::{get_animal_by_period, health_check};
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
@@ -14,7 +14,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
         App::new()
             .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            // .route("/subscriptions", web::post().to(subscribe))
             .route(
                 "/api/analytics/animals/getAnimalCountByKindForPeriod",
                 web::get().to(get_animal_by_period),
