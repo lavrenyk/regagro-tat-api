2020963d3cd733118676c3a3f68ce57948c4a972
yc work-instance <lavrenyk@gmail.com>
2023-11-02 20:19:57 +0000

    ready to get data from db!

--- src/routes/get_animal_by_period.rs
+++ src/routes/get_animal_by_period.rs
@@ -1,7 +1,6 @@
 //! src/routes/get_animal_by_period.rs
 
 use actix_web::{web, HttpResponse};
-use async_std::task;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::{Error, MySql, MySqlPool, Pool};
@@ -15,82 +14,72 @@ pub struct Field {
     pub districts: String,
 }
 
-// Testing connection to MySQL
-///
-/// It should be connect well
-async fn connect() -> Result<Pool<MySql>, Error> {
-    return MySqlPool::connect(
-        "mysql://mp_analytic:8Nlr7fDQNwmniu6h@vo.regagro.ru:33633/regagro_3_0",
-    )
-    .await;
-}
+pub async fn get_animal_by_period(data: web::Query<Field>) -> HttpResponse {
 
-async fn do_run_query() {
-    let result = task::block_on(connect());
-    let district_code = "bb9c86eb-30de-4b8f-9ea8-9edc71fc0488".to_string();
-
-    let query = format!(
+  let connection = MySqlPool::connect("mysql://mp_analytic:8Nlr7fDQNwmniu6h@vo.regagro.ru:33633/regagro_3_0").await;
+    
+  let district_code = "bb9c86eb-30de-4b8f-9ea8-9edc71fc0488".to_string();
+  let query = format!(
       "SELECT a.id FROM animals AS a LEFT JOIN enterprises AS e ON a.enterprise_id=e.id LEFT JOIN enterprise_addresses AS ea ON ea.enterprise_id=e.id WHERE ea.district_code='{}';", district_code
   );
 
-    dbg!(&query);
+  dbg!(&connection);
+  dbg!(&data);
 
-    match result {
-        Err(err) => {
-            println!("Cannot connect to database [{}]", err.to_string());
-        }
+  match connection {
+      Err(err) => {
+          println!("Cannot connect to database [{}]", err.to_string());
+      }        
+      Ok(pool) => {
+          println!("Connected to database successfully.");
+          let query_result = sqlx::query(
+              query.as_str())
+              .fetch_all(&pool).await.unwrap();
 
-        Ok(pool) => {
-            let query_result = sqlx::query(query.as_str()).fetch_all(&pool).await.unwrap();
+          println!("Number of Animals selected: {:?}", query_result.len());
+      }
+  }
 
-            println!("Number of Animals selected: {:?}", query_result.len());
-        }
-    }
-}
-
-pub async fn get_animal_by_period(data: web::Query<Field>) -> HttpResponse {
-    task::block_on(do_run_query());
-
-    let body = json!(
-      [
-        {
-          "count": 31990,
-          "kind_id": 1,
-          "view": "КРС",
-          "name": "Крупный рогатый скот"
-        },
-        {
-          "count": 23,
-          "kind_id": 2,
-          "view": "Свинья",
-          "name": "Свинья"
-        },
-        {
-          "count": 17,
-          "kind_id": 3,
-          "view": "Лошади",
-          "name": "Лошади"
-        },
-        {
-          "count": 10,
-          "kind_id": 7,
-          "view": "Олени",
-          "name": "Олени"
-        },
-        {
-          "count": 49,
-          "kind_id": 12,
-          "view": "Овцы",
-          "name": "Овцы"
-        },
-        {
-          "count": 138,
-          "kind_id": 13,
-          "view": "Козы",
-          "name": "Козы"
-        }
-      ]
-    );
+  // let body = json!(
+  //     [
+  //       {
+  //         "count": 31990,
+  //         "kind_id": 1,
+  //         "view": "КРС",
+  //         "name": "Крупный рогатый скот"
+  //       },
+  //       {
+  //         "count": 23,
+  //         "kind_id": 2,
+  //         "view": "Свинья",
+  //         "name": "Свинья"
+  //       },
+  //       {
+  //         "count": 17,
+  //         "kind_id": 3,
+  //         "view": "Лошади",
+  //         "name": "Лошади"
+  //       },
+  //       {
+  //         "count": 10,
+  //         "kind_id": 7,
+  //         "view": "Олени",
+  //         "name": "Олени"
+  //       },
+  //       {
+  //         "count": 49,
+  //         "kind_id": 12,
+  //         "view": "Овцы",
+  //         "name": "Овцы"
+  //       },
+  //       {
+  //         "count": 138,
+  //         "kind_id": 13,
+  //         "view": "Козы",
+  //         "name": "Козы"
+  //       }
+  //     ]
+  //   );
 
     let json_message = json!({
         "region_id": data.region_id,
@@ -100,9 +89,9 @@ pub async fn get_animal_by_period(data: web::Query<Field>) -> HttpResponse {
         "districts": data.districts
     });
 
-    println!("path data: {:?}", json_message);
+    // println!("path data: {:?}", json_cmessage);
 
     HttpResponse::Ok()
         .content_type("application/json")
-        .json(body)
+        .json(json_message)
 }
--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,7 +1,31 @@
 //! src/routes/health_check.rs
 
 use actix_web::HttpResponse;
+use sqlx::{Error, MySql, MySqlPool, Pool};
 
 pub async fn health_check() -> HttpResponse {
+    let connection = MySqlPool::connect("mysql://mp_analytic:8Nlr7fDQNwmniu6h@vo.regagro.ru:33633/regagro_3_0").await;
+    
+    let district_code = "bb9c86eb-30de-4b8f-9ea8-9edc71fc0488".to_string();
+    let query = format!(
+        "SELECT a.id FROM animals AS a LEFT JOIN enterprises AS e ON a.enterprise_id=e.id LEFT JOIN enterprise_addresses AS ea ON ea.enterprise_id=e.id WHERE ea.district_code='{}';", district_code
+    );
+
+    dbg!(&connection);
+
+    match connection {
+        Err(err) => {
+            println!("Cannot connect to database [{}]", err.to_string());
+        }        
+        Ok(pool) => {
+            println!("Connected to database successfully.");
+            let query_result = sqlx::query(
+                query.as_str())
+                .fetch_all(&pool).await.unwrap();
+
+            println!("Number of Animals selected: {:?}", query_result.len());
+        }
+    }
+
     HttpResponse::Ok().finish()
 }
