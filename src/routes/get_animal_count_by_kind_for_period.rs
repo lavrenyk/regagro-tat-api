a2b61a89cb33edbca4749a0a4996940774e1ff6b
ārXāngel MacBook <lavrenyk@gmail.com>
2023-11-07 16:09:50 +0300

    minor changes

--- src/routes/get_animal_count_by_kind_for_period.rs
+++ src/routes/get_animal_count_by_kind_for_period.rs
@@ -2,8 +2,9 @@
 
 use crate::helpers::{all_districts_filter, animals_filter_query, district_filter_query};
 use actix_web::{web, HttpResponse};
+use async_std::stream::StreamExt;
 use serde::{Deserialize, Serialize};
-use sqlx::{query_builder::QueryBuilder, Execute, MySqlConnection, MySqlPool};
+use sqlx::{query_builder::QueryBuilder, Column, Execute, MySqlConnection, MySqlPool, Row};
 use tracing::Instrument;
 use uuid::Uuid;
 
@@ -36,8 +37,6 @@ pub async fn get_animal_count_by_kind_for_period(
     let request_id = Uuid::new_v4();
     let query_span = tracing::info_span!("Requesting animals count by kinds for period");
 
-    dbg!(&data);
-
     // ЭТАП 1: Переформатируем QueryData в необходимые данные для работы
     // 1. Определяем количество видов в запросе. Для этого переводим данные QueryData.kinds
     // из [`String`] в [`Vec<u8>`]. Дополнительно проверяем, были ли данные по в запросе по
@@ -54,8 +53,6 @@ pub async fn get_animal_count_by_kind_for_period(
         }
     }
 
-    dbg!(animal_kinds);
-
     // 2. Переводим QueryData.districts из [`String`] в [`Vec<String>`] с параллельной конвертацией
     // числового значения в GUID региона и готовой части фильтра для запроса. Если в запросе нет данных,
     // то выбираем все регионы
@@ -69,45 +66,42 @@ pub async fn get_animal_count_by_kind_for_period(
         }
     }
 
-    let connection = MySqlPool::connect("mysql://root:kakeepoo@127.0.0.1:3306/regagro_3_0").await;
-    let query = format!("SELECT created_at FROM animals LIMIT 2");
+    let query = format!("SELECT id FROM animals LIMIT 10");
 
-    match connection {
-        Err(err) => {
-            println!("Cannot connect to database [{}]", err.to_string());
-        }
-        Ok(pool) => {
-            println!("Connected to database successfully.");
-            let query_result = sqlx::query(query.as_str()).fetch_all(&pool).await.unwrap();
-            dbg!(query_result);
-        }
-    }
-
-    let query = format!(
-        "SELECT a.id FROM animals AS a LEFT JOIN enterprises AS e ON a.enterprise_id=e.id LEFT JOIN enterprise_addresses AS ea ON ea.enterprise_id=e.id WHERE a.kind_id=1 AND ({});",
-        &districts_filter
-    );
-
-    match sqlx::query(
-        &query, // r#"
-               // SELECT COUNT(*) as count FROM animals AS a
-               // LEFT JOIN enterprises AS e ON a.enterprise_id=e.id
-               // LEFT JOIN enterprise_addresses AS ea ON ea.enterprise_id=e.id WHERE a.kind_id=1 AND ea.district_code = "'2f40b5af-a9f6-4056-8581-48c9d5c44e7e'"
-               // "#,        // districts_filter
+    let result = sqlx::query(
+        r#"
+            SELECT COUNT(*) as count FROM animals;
+        "#,
     )
-    .execute(pool.as_ref())
-    .instrument(query_span)
-    .await
-    {
-        Ok(data) => {
-            dbg!(data);
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("{} - Failed to execute query: {:?}", request_id, e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .fetch_all(pool.as_ref())
+    .await;
 
-    // HttpResponse::Ok().finish()
+    let result2: Result<(i64,), _> = sqlx::query_as(
+        r#"
+            SELECT COUNT(*) as count FROM animals;
+        "#,
+    )
+    .fetch_one(pool.as_ref())
+    .await;
+
+    // let new_res: u64 = &result.unwrap()[0].try_get("count");
+    // dbg!(&result.unwrap()[0].try_get::<u64, _>("count"));
+    dbg!(&result2.unwrap().0);
+
+    // match sqlx::query(&query)
+    //     .fetch_all(pool.as_ref())
+    //     // .instrument(query_span)
+    //     .await
+    // {
+    //     Ok(data) => {
+    //         dbg!(data);
+    //         HttpResponse::Ok().finish()
+    //     }
+    //     Err(e) => {
+    //         tracing::error!("{} - Failed to execute query: {:?}", request_id, e);
+    //         HttpResponse::InternalServerError().finish()
+    //     }
+    // }
+
+    HttpResponse::Ok().finish()
 }
